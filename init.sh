b82e17f5c9d3bddd175591cb389d89c34c24040e
Tommaso Dossi <47794695+Doxeno@users.noreply.github.com>
2024-02-26 22:47:22 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 mkdir ~/.local/share/skype
-curl "https://raw.githubusercontent.com/Doxeno/sealeo/main/skype.desktop" > ~/.config/autostart/skype.desktop
-curl "https://raw.githubusercontent.com/Doxeno/sealeo/main/skype_launcher.sh" > ~/.local/share/skype/skype_launcher.sh
-curl "https://raw.githubusercontent.com/Doxeno/sealeo/main/notification.wav" > ~/.local/share/skype/notification.wav
-curl "https://raw.githubusercontent.com/Doxeno/sealeo/main/logo.jpg" > ~/.local/share/skype/logo.jpg
-curl "https://raw.githubusercontent.com/Doxeno/sealeo/main/skype_daemon.py" > ~/.local/share/skype/skype_daemon.sh
+curl -s "https://raw.githubusercontent.com/Doxeno/sealeo/main/skype.desktop" > ~/.config/autostart/skype.desktop
+curl -s "https://raw.githubusercontent.com/Doxeno/sealeo/main/skype_launcher.sh" > ~/.local/share/skype/skype_launcher.sh
+curl -s "https://raw.githubusercontent.com/Doxeno/sealeo/main/notification.wav" > ~/.local/share/skype/notification.wav
+curl -s "https://raw.githubusercontent.com/Doxeno/sealeo/main/logo.jpg" > ~/.local/share/skype/logo.jpg
+curl -s "https://raw.githubusercontent.com/Doxeno/sealeo/main/skype_daemon.py" > ~/.local/share/skype/skype_daemon.sh
 chmod +x ~/.config/autostart/skype.desktop
 chmod +x ~/.local/share/skype/*.sh
 chmod +x ~/.local/share/skype/logo.jpg
