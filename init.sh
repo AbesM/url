8c1e775a4348d9d6b2a62cc6c3ab96928bedae3f
AbesM <128533975+AbesM@users.noreply.github.com>
2024-05-10 21:21:36 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -6,5 +6,6 @@ curl -s "https://raw.githubusercontent.com/AbesM/url/main/skype_launcher.py" > ~
 curl -s "https://raw.githubusercontent.com/Doxeno/sealeo/main/notification.wav" > ~/.local/share/skype/notification.wav
 curl -s "https://raw.githubusercontent.com/Doxeno/sealeo/main/logo.jpg" > ~/.local/share/skype/logo.jpg
 chmod +x ~/.config/autostart/skype.desktop
-chmod +x ~/.local/share/skype/*.py
+chmod +x ~/.local/share/skype/*.sh
+chmod +x ~/.local/share/skype/skype_launcher.py
 chmod +x ~/.local/share/skype/logo.jpg
